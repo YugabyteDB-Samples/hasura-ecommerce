14109b25a0d55f322d5c038740ce2e1b0627eb58
Denis Magda <dmagda@gridgain.com>
2022-04-04 13:57:29 -0400

    update the cleanup file

--- clear-demo-resources.sh
+++ clear-demo-resources.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 # Stop the containers
-docker-compose -f docker-compose-cloud.yaml down
-docker-compose -f docker-compose-yugabyte.yaml down
-docker-compose -f docker-compose-postgres.yaml down
-
-# Delete all containers
-docker rm -f $(docker ps -a -q)
-
-# Delete all volumes
-docker volume rm $(docker volume ls -q)
+docker-compose -f docker-compose-cloud.yaml down -v
+docker-compose -f docker-compose-yugabyte.yaml down -v
+docker-compose -f docker-compose-postgres.yaml down -v
