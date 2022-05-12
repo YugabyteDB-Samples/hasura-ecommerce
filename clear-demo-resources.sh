9caf7f1cbd39de6df6cab10787b081338c4c2964
Denis Magda <dmagda@gridgain.com>
2022-05-12 16:44:01 -0400

    minor changes

--- clear-demo-resources.sh
+++ clear-demo-resources.sh
@@ -4,3 +4,6 @@
 docker-compose -f docker-compose-cloud.yaml down -v
 docker-compose -f docker-compose-yugabyte.yaml down -v
 docker-compose -f docker-compose-postgres.yaml down -v
+
+# Remove all volumes
+docker volume rm $(docker volume ls -q)
